9c3c3636133b6432b931ee0cd38bccb624bc0e7f
Matti Lehtinen <matlehti@gmail.com>
2015-03-28 16:56:46 +0200

    Handle errors on startup and accept connections to all ipv4 address instead of just localhost.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 var GameServer = require("./lib/game-server.js");
 
-var HOST = '127.0.0.1';
+var HOST = undefined; // Accept connections to any IPv4 address
 var SOCKET_PORT = 6969;
 var WEB_SOCKET_PORT = 3000;
 
 var gameServer = new GameServer(HOST, SOCKET_PORT, WEB_SOCKET_PORT);
 gameServer.start(function(err) {
+    if(err) {
+        return console.log(err);
+    }
     console.log("Game server fully started.");
 });
